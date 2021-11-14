775bad117f5de489cb087694bddf90c1ca8020d9
parra06 <santiago.parra.0276@eam.edu.co>
2021-11-14 16:26:29 -0500

    jiji

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -14,37 +14,53 @@ public class Ejercicio3 {
 
     public static void main(String[] args) {
         int tamano = Integer.parseInt(JOptionPane.showInputDialog("Ingrese tamaño deseado")) + 1;
-        int[] matriz = new int[tamano];
-        puntoB(matriz);
+        int[][] matriz = new int[tamano][tamano];
+        puntoB(matriz, 0, 0);
     }
 
     /**
      * Metodo Recursivo
      */
-    public static void puntoB(int[] arreglo) {
-        int columnas = arreglo.length;
-        for (int n = 0; n < columnas; n++) {
-            int numero = 3;
-            System.out.format("%" + (columnas - n) * 2 + "s", "");//formato
-            for (int k = 0; k <= n; k++) {
-                System.out.format("%4d", numero);
-                numero = numero * (n - k) / (k + 1);
+    public static void puntoB(int[][] matriz, int i, int j) {
+        int filas = matriz.length;
+        if (j <= i) {
+            if (i < filas && j == 0) {
+                matriz[i][j] = 3;
+                System.out.format("%4d", matriz[i][j]);
+            } else if (i == j) {
+                matriz[i][j] = 3;
+                System.out.format("%4d", matriz[i][j]);
+            } else {
+                matriz[i][j] = matriz[i][j - 1] + matriz[i - 1][j] + 2;
+                System.out.format("%4d", matriz[i][j]);
             }
-            System.out.println();
+            puntoB(matriz, i, j + 1);
+        } else {
+            if (i < filas - 1) {
+                System.out.println();
+                puntoB(matriz, i + 1, 0);
+            }
         }
     }
 
     /**
      * Metodo Tabulación
      */
-    public static void puntoC(int[] arreglo) {
-        int columnas = arreglo.length;
-        for (int n = 0; n < columnas; n++) {
-            int numero = 3;
-            System.out.format("%" + (columnas - n) * 2 + "s", "");//formato
-            for (int k = 0; k <= n; k++) {
-                System.out.format("%4d", numero);
-                numero = numero * (n - k) / (k + 1);
+    public static void puntoC(int[][] matriz) {
+        int filas = matriz.length;
+        for (int i = 0; i < filas; i++) {
+            System.out.format("%" + (filas - i) * 2 + "s", "");//formato
+            for (int j = 0; j <= i; j++) {
+                if (i < filas && j == 0) {
+                    matriz[i][j] = 3;
+                    System.out.format("%4d", matriz[i][j]);
+                } else if (i == j) {
+                    matriz[i][j] = 3;
+                    System.out.format("%4d", matriz[i][j]);
+                } else {
+                    matriz[i][j] = matriz[i][j - 1] + matriz[i - 1][j] + 2;
+                    System.out.format("%4d", matriz[i][j]);
+                }
             }
             System.out.println();
         }
